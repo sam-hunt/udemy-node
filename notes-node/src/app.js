e310baa064c5c3b6c378ee2d6b39cff8f0e4e07b
Sam Hunt <s2112201@ipc.ac.nz>
2016-11-16 23:39:10 +1300

    Udemy section 3 lecture 12. remove test functionality.

--- notes-node/src/app.js
+++ notes-node/src/app.js
@@ -1,20 +1,6 @@
 console.log('Starting app.js');
 
 const fs = require('fs');
-const os = require('os');
 const _ = require('lodash');
 
 const notes = require('./notes.js');
-
-
-
-// console.log(_.isString(true));
-// console.log(_.isString('Sam'));
-
-var filteredArray = _.uniq(['Sam', 1, 'Sam', 1, 2, 3, 4]);
-console.log(filteredArray);
-
-//console.log('Result: ', notes.add(4, 7));
-
-// var user = os.userInfo();
-// fs.appendFileSync('../out/greetings.txt',`Hello ${user.username}! You are ${notes.age}.`);
