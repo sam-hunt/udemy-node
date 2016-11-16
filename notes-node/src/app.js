fd330dee1c5dbe7160d7920fa8913b5c46e757e2
Sam Hunt <s2112201@ipc.ac.nz>
2016-11-16 23:32:47 +1300

    Udemy section 3 lecture 11. Use npm.

--- notes-node/src/app.js
+++ notes-node/src/app.js
@@ -2,9 +2,19 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
+
 const notes = require('./notes.js');
 
-console.log('Result: ', notes.add(4, 7));
+
+
+// console.log(_.isString(true));
+// console.log(_.isString('Sam'));
+
+var filteredArray = _.uniq(['Sam', 1, 'Sam', 1, 2, 3, 4]);
+console.log(filteredArray);
+
+//console.log('Result: ', notes.add(4, 7));
 
 // var user = os.userInfo();
 // fs.appendFileSync('../out/greetings.txt',`Hello ${user.username}! You are ${notes.age}.`);
