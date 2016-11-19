4ab00d54644c0138b50a9e440e0af75e89c11843
Sam Hunt <s2112201@ipc.ac.nz>
2016-11-20 11:02:49 +1300

    Udemy sec3 lec17. Refactor common io functionality.

--- notes-node/src/app.js
+++ notes-node/src/app.js
@@ -14,7 +14,15 @@ console.log('Yargs', argv);
 
 if (command === 'add') {
     console.log('Adding new note');
-    notes.addNote(argv.title, argv.body);
+    var note = notes.addNote(argv.title, argv.body);
+    if (note) {
+        console.log('Note created');
+        console.log('--');
+        console.log(`Title: ${note.title}`);
+        console.log(`Body: ${note.body}`);
+    } else {
+        console.log('Note title taken');
+    }
 } else if (command === 'list') {
     notes.getAll();
 } else if (command === 'read') {
