ec3f3228a57d2fe37edafb4d283489f208c791cb
Sam Hunt <s2112201@ipc.ac.nz>
2016-11-23 08:07:30 +1300

    Udemy sec4 lect30. Parse HTTP response.

--- weather-app/app.js
+++ weather-app/app.js
@@ -5,5 +5,11 @@ request({
     url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
     json: true
 }, (error, response, body) => {
-    console.log(body);
-})
+    //console.log(JSON.stringify(body, undefined, 4));
+    //console.log(JSON.stringify(error, undefined, 4));
+
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+});
+
