f711c5e492e10ba4065e76b0e7619c2beecaf308
Sam Hunt <s2112201@ipc.ac.nz>
2016-11-25 07:56:38 +1300

    Udemy sec4 lec 32. Add simple error handling.

--- weather-app/app.js
+++ weather-app/app.js
@@ -22,10 +22,15 @@ request({
     json: true
 }, (error, response, body) => {
     //console.log(JSON.stringify(body, undefined, 4));
-    //console.log(JSON.stringify(error, undefined, 4));
-
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+    
+    if (error) {
+        console.log('Unable to connect to Google servers.');
+    } else if (body.status === 'ZERO_RESULTS') {
+        console.log('Unable to find that address.');
+    } else if (body.status === 'OK') {
+        console.log(`Address: ${body.results[0].formatted_address}`);
+        console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+        console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+    }
 });
 
