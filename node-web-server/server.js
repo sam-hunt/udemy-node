fc6cfcd248147608af45d81972609eaf102df7e4
Sam Hunt <s2112201@ipc.ac.nz>
2016-12-16 08:03:52 +1300

    Udemy sec5 lec 42. Creating a web server.

---
+++ node-web-server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+var app = express();
+
+app.get('/', (req, res) => {
+    //res.send('<h1>Hello Express!</h1>');
+    res.send({
+        name: 'Sam',
+        likes: [
+            'python',
+            'java',
+            'sql'
+        ]
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.send('About page');
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Unable to handle request'
+    });
+});
+
+app.listen(3000);
