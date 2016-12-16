fd8303f0271b34ecb676de53da4c3be2d4e0c61e
Sam Hunt <s2112201@ipc.ac.nz>
2016-12-17 00:22:31 +1300

    Udemy sec5 lec44. Advanced templating. Implement partials and helpers.

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -3,21 +3,30 @@ const hbs     = require('hbs');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
+// Register helpers
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+
+// Register routes
 app.get('/', (req, res) => {
      res.render('home.hbs', {
         pageTitle      : 'Home Page',
-        currentYear    : new Date().getFullYear(),
         welcomeMessage : 'Welcome to my website'
     });
 });
 
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
-        pageTitle: 'About Page',
-        currentYear: new Date().getFullYear()
+        pageTitle: 'About Page'
     });
 });
 
