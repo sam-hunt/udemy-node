4ec0daef6665ea5682d4f37bd928044cfd4f02b6
Sam Hunt <s2112201@ipc.ac.nz>
2016-12-16 18:54:05 +1300

    Udemy sec5 lec42-43. HBS templates and static html.

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,21 +1,24 @@
 const express = require('express');
+const hbs     = require('hbs');
 
 var app = express();
 
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
-    //res.send('<h1>Hello Express!</h1>');
-    res.send({
-        name: 'Sam',
-        likes: [
-            'python',
-            'java',
-            'sql'
-        ]
+     res.render('home.hbs', {
+        pageTitle      : 'Home Page',
+        currentYear    : new Date().getFullYear(),
+        welcomeMessage : 'Welcome to my website'
     });
 });
 
 app.get('/about', (req, res) => {
-    res.send('About page');
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear()
+    });
 });
 
 app.get('/bad', (req, res) => {
@@ -24,4 +27,6 @@ app.get('/bad', (req, res) => {
     });
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+    console.log('Server is up on port 3000')
+});
